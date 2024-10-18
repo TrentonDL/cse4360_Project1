693947d6867ba8e534dd2bba160ca7d361e25824
TrentonDL <trenton.laule@mavs.uta.edu>
2024-10-18 14:20:27 -0500

    Define motor variables

--- project1_team5.py
+++ project1_team5.py
@@ -1,3 +1,8 @@
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Port,Direction
 from pybricks.tools import wait
+
+#Define Motors and their port connections
+l_Motor = Motor(Port.A,Direction.COUNTERCLOCKWISE)
+r_Motor = Motor(Port.B, Direction.CLOCKWISE)
+
