75e47771a6ed6ab1ca43cf26f1d6fe779ed8171c
TrentonDL <trenton.laule@mavs.uta.edu>
2024-10-24 10:09:24 -0500

    working on the dead reckoning algorithm

---
+++ dead_reckoning.py
@@ -0,0 +1,20 @@
+import math
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port,Direction
+from pybricks.tools import wait
+
+def dead_reckoning(l_motor=Motor, r_motor=Motor, pos_x=float, pos_y=float, theta=float):
+    l_speed = l_motor.speed
+    r_speed = r_motor.speed
+    wheel_radius = 1.0
+    b = 5.0
+
+    pos_x = (wheel_radius*l_speed + wheel_radius*r_speed) * math.cos(theta)/2
+    pos_y = (wheel_radius*l_speed + wheel_radius*r_speed) * math.sin(theta)/2
+    theta = (wheel_radius*l_speed - wheel_radius*r_speed) / b
+
+    velocity = math.sqrt(math.pow(pos_x,2)+math.pow(pos_y,2))
+
+    return [pos_x,pos_y,theta,velocity]
+
+
