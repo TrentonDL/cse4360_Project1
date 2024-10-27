35430a5536a18409e4e117980ce3c914fcb6f142
TrentonDL <trenton.laule@mavs.uta.edu>
2024-10-27 15:04:04 -0500

    update dead reckoning to correctly update the robots position and heading

--- dead_reckoning.py
+++ dead_reckoning.py
@@ -4,18 +4,21 @@ from pybricks.pupdevices import Motor
 from pybricks.parameters import Port,Direction
 from pybricks.tools import wait
 
-def dead_reckoning(l_motor=Motor, r_motor=Motor, pos_x=float, pos_y=float, theta=float):
+def dead_reckoning(hub=PrimeHub,l_motor=Motor,r_motor=Motor, pos_x=float, pos_y=float, theta=float):
+    robot_heading = hub.imu.heading
     l_speed = l_motor.speed
     r_speed = r_motor.speed
+    theta_dot = robot_heading-theta
     wheel_radius = 1.0
     b = 5.0
 
-    pos_x = (wheel_radius*l_speed + wheel_radius*r_speed) * math.cos(theta)/2
-    pos_y = (wheel_radius*l_speed + wheel_radius*r_speed) * math.sin(theta)/2
-    theta = (wheel_radius*l_speed - wheel_radius*r_speed) / b
+    x_dot = (wheel_radius*l_speed + wheel_radius*r_speed) * math.cos(theta)/2
+    y_dot = (wheel_radius*l_speed + wheel_radius*r_speed) * math.sin(theta)/2
 
-    velocity = math.sqrt(math.pow(pos_x,2)+math.pow(pos_y,2))
+    pos_x += x_dot
+    pos_y += y_dot
+    theta += theta_dot
 
-    return [pos_x,pos_y,theta,velocity]
+    return pos_x, pos_y, theta
 
 
